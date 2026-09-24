0eaf49b185376b0d7a2dcf46925471bc9acafbf1
figo kurniawan <kurniawanfigo25@gmail.com>
2026-09-24 16:05:43 +0700

    favicon

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -7,6 +7,17 @@
         name="description"
         content="{{ $metaDescription ?? 'Pusat pelatihan dan jasa K3 terpercaya di seluruh Indonesia.' }}" />
     <link rel="canonical" href="{{ url()->current() }}" />
+
+    {{-- Favicon (SVG untuk browser modern, PNG/ICO sebagai fallback + PWA/Google
+    search result icon, apple-touch-icon untuk iOS home screen) --}}
+    <link rel="icon" href="{{ asset('favicon.svg') }}" type="image/svg+xml" />
+    <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any" />
+    <link rel="icon" href="{{ asset('favicon-16x16.png') }}" type="image/png" sizes="16x16" />
+    <link rel="icon" href="{{ asset('favicon-32x32.png') }}" type="image/png" sizes="32x32" />
+    <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}" sizes="180x180" />
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}" />
+    <meta name="theme-color" content="#0b253d" />
+
     @isset($city)
         <meta name="robots" content="noindex, follow" />
     @else
